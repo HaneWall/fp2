a770e61c05b66e21d7a2a12ebbd07665da4809b0
HaneWall <48854946+HaneWall@users.noreply.github.com>
2020-08-16 13:50:17 +0200

    peak_finder

--- mass_spectra/plots.py
+++ mass_spectra/plots.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from scipy.signal import find_peaks
 import numpy as np
 
 # Colors
@@ -48,14 +49,25 @@ def show_both():
     u_s = read_data('sf6.txt')[0]
     i_s = read_data('sf6.txt')[1]
 
+    peaks_sf6_indices = find_peak(data=i_s, minimal_height=1.08)[0]
+    print(peaks_sf6_indices)
+    peak_i = [i_s[index] for index in peaks_sf6_indices]
+    peak_u = [u_s[index] for index in peaks_sf6_indices]
+
     fig, axes = plt.subplots()
     axes.plot(u_k, i_k, color='GREY', label='Krypton')
     axes.plot(u_s, i_s, color='TEAL', label=r'$SF_6$')
+    axes.plot(peak_u, peak_i, color='RED', linestyle='None', marker='+')
     axes.grid(True, color='black', linestyle='dashed', alpha=0.2)
     axes.set_xlabel(r'$U$ in $V$')
     axes.set_ylabel(r'Intensität $I$')
     axes.legend(loc='best')
     plt.show()
+
+def find_peak(data, minimal_height):
+    indices = find_peaks(x=data, height=minimal_height)
+    return indices
+
 
 #show_sf6()
 #show_krypton()
