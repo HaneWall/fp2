25955c41ed89db5a5cbee8bc0b0bafcdc30a0efa
HaneWall <48854946+HaneWall@users.noreply.github.com>
2020-08-19 16:18:16 +0200

    FIXED: relative Paths CHANGED: show_all() labels, colors

--- roentgen/plot3.py
+++ roentgen/plot3.py
@@ -13,6 +13,9 @@ RED = '#CC3311'
 MAGENTA = '#EE3377'
 GREY = '#BBBBBB'
 
+color_spec = [BLUE, CYAN, TEAL, ORANGE, RED, MAGENTA, GREY, 'black']
+energy_duane = [str(9 + 3*i) for i in range(8)]
+
 #test
 def read_data(path):
 	data = pd.read_csv(path, sep='\t', decimal='.', header=None)
@@ -23,17 +26,17 @@ def show_all():
 	theta = []
 	n = []
 	for i in range(9, 33, 3):
-		theta.append(read_data('/Users/erikweilandt/Documents/University/fp2/roentgen/resources/lif_'+str(i)+'_10_01_4_.txt')[0])
-		n.append(read_data('/Users/erikweilandt/Documents/University/fp2/roentgen/resources/lif_'+str(i)+'_10_01_4_.txt')[1])
+		theta.append(read_data('resources/lif_'+str(i)+'_10_01_4_.txt')[0])
+		n.append(read_data('resources/lif_'+str(i)+'_10_01_4_.txt')[1])
 
 	fig, axes = plt.subplots()
 	for j in range(0, 8):
-		axes.plot(theta[j], n[j], color='GREY', label='Krypton')
+		axes.plot(theta[j], n[j], color=color_spec[j], label=energy_duane[j] + ' keV')
 	# axes.plot(theta30, n30, color='TEAL', label=r'$SF_6$')
 	# axes.plot(peak_u, peak_i, color='RED', linestyle='None', marker='+')
 	axes.grid(True, color='black', linestyle='dashed', alpha=0.2)
-	axes.set_xlabel(r'$U$ in $V$')
-	axes.set_ylabel(r'Intensität $I$')
+	axes.set_xlabel(r'$\theta$ in °')
+	axes.set_ylabel(r'Impulse')
 	axes.legend(loc='best')
 	plt.show()
 
