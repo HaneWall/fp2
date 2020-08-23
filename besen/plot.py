6b2bd6e116ede0d078d161b973d9f1814b09820e
ErikLahm <eriklahm@freenet.de>
2020-08-23 21:43:12 +0200

    edit

--- besen/plot.py
+++ besen/plot.py
@@ -16,6 +16,10 @@ def read_data(path):
     df = pd.read_csv('./Besen_daten/'+path, sep=';', decimal=',')
     return df.to_numpy()
 
+def read_data_task3(path):
+    df = pd.read_csv('./Besen_daten/' + path, sep=';', decimal=',')
+    return df
+
 def show_spectrum_sensor():
     spectrum_sensor = np.transpose(read_data('Blatt 1-Tabelle 1.csv'))
     fig, axes = plt.subplots()
@@ -25,5 +29,19 @@ def show_spectrum_sensor():
     axes.set_ylabel(r'$U_{sensor}$')
     plt.show()
 
+def show_table_task3():
+    data_table = read_data_task3('Blatt 4-Tabelle 1.txt')
+    U_0 = read_data_task3('Blatt 2-Tabelle 1.txt')
+    for i in range(1, 4):
+        data_table.loc[10, 'U_d'+str(i)] = data_table['U_d'+str(i)].mean()
+    data_table['U_0'] = U_0
+    data_table.loc[10, 'U_0'] = data_table['U_0'].mean()*10
+    print(data_table.to_latex())
+    data_table = data_table.to_numpy()
+    print(data_table[-1])
+    results = 20*np.log10(data_table[-1][0:3]/(10*data_table[-1][3]))
+    print(results)
 
-show_spectrum_sensor()
+
+#show_spectrum_sensor()
+show_table_task3()
