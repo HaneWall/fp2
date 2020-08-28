295c67b7746b297a54d2cc1e552a8699b9d21be4
ErikLahm <eriklahm@freenet.de>
2020-08-28 18:46:49 +0200

    QuA

---
+++ qua/plot.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+BLUE = '#0077BB'
+CYAN = '#33BBEE'
+TEAL = '#009988'
+ORANGE = '#EE7733'
+RED = '#CC3311'
+MAGENTA = '#EE3377'
+GREY = '#BBBBBB'
+
