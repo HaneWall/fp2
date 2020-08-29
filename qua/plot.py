7ddf1127005c9fea044eb7b0fbef56361f14cff7
ErikLahm <eriklahm@freenet.de>
2020-08-29 13:30:20 +0200

    all tasks for now

--- qua/plot.py
+++ qua/plot.py
@@ -23,31 +23,32 @@ def read_polar_data(path):
 	return df
 
 
-def show_spec(path, labels):
-	if path is list:
-		i = 0
-		fig, axes = plt.subplots(ncols=1, nrows=len(path))
-		for path in path:
-			frequence = read_data(path)[:, 0]
-			amplitude = read_data(path)[:, 1]
-
-			axes[i].plot(frequence, amplitude, color=TEAL, marker='+', label=labels[i])
-			axes[i].grid(True, color='black', linestyle='dashed', alpha=0.2)
-			axes[i].set_xlabel('frequence in Hz')
-			axes[i].set_ylabel(r'amplitude')
-			axes[i].legend(loc='best')
-			i = i + 1
-
-	else:
+def show_spec_list(path, labels):
+	i = 0
+	fig, axes = plt.subplots(ncols=1, nrows=len(path))
+	for path in path:
 		frequence = read_data(path)[:, 0]
 		amplitude = read_data(path)[:, 1]
 
-		fig, axes = plt.subplots()
-		axes.plot(frequence, amplitude, color=TEAL, marker='+', label=labels)
-		axes.grid(True, color='black', linestyle='dashed', alpha=0.2)
-		axes.set_xlabel('frequence in Hz')
-		axes.set_ylabel(r'amplitude')
-		axes.legend(loc='best')
+		axes[i].plot(frequence, amplitude, color=TEAL, marker='+', label=labels[i])
+		axes[i].grid(True, color='black', linestyle='dashed', alpha=0.2)
+		axes[i].set_xlabel('frequence in Hz')
+		axes[i].set_ylabel(r'amplitude')
+		axes[i].legend(loc='best')
+		i = i + 1
+	plt.show()
+
+
+def show_spec_single(path, labels):
+	frequence = read_data(path)[:, 0]
+	amplitude = read_data(path)[:, 1]
+
+	fig, axes = plt.subplots()
+	axes.plot(frequence, amplitude, color=TEAL, marker='+', label=labels)
+	axes.grid(True, color='black', linestyle='dashed', alpha=0.2)
+	axes.set_xlabel('frequence in Hz')
+	axes.set_ylabel(r'amplitude')
+	axes.legend(loc='best')
 	plt.show()
 
 
@@ -59,7 +60,7 @@ def peak_alpha_plot(path):
 	peak1 = np.array([39.938, 33.978, 32.498, 33.404, 28.64, 18.582, 12.634, 27.43, 42.152, 53.847])
 	peak2 = np.array([17.01, 20.806, 20.781, 24.253, 27.508, 28.312, 25.302, 26.384, 52.119, 58.032])
 	alpha = pd.DataFrame([0, 10, 30, 50, 70, 90, 110, 130, 150, 180])  # TODO: edit alpha to theta
-	alpha[0] = np.arccos(1 / 2 * (np.cos(alpha[0]) - 1))
+	alpha[0] = np.arccos(1 / 2 * (np.cos(alpha[0]) - 1)) #TODO: is wrong
 	fig, axes = plt.subplots()
 	axes.plot(alpha[0], peak1, color=TEAL, marker='+')
 	axes.plot(alpha[0], peak2, color=RED, marker='*')
@@ -88,6 +89,35 @@ def show_polar_plots(path, labels):
 		axes.legend(loc='best')
 		m = m + 1
 	plt.show()
+
+def show_spec_with_peaks(path):
+	frequence = read_data(path)[:, 0]
+	amplitude = read_data(path)[:, 1]
+	peak_index, _ = find_peak(amplitude, 1)
+
+	peak_freq = [frequence[index] for index in peak_index]
+	del peak_freq[0:2]
+	peak_amp = [amplitude[index] for index in peak_index]
+	del peak_amp[0:2]
+	data_table = pd.DataFrame(peak_freq)
+	data_table[0] = data_table[0] * 2 * np.pi
+	data_table['k'] = data_table[0] / 340.27 #TODO: right?
+	print(data_table)
+
+	fig, axes = plt.subplots(nrows=2, ncols=1)
+	axes[0].plot(frequence, amplitude, color=TEAL, marker='+', label='600mm with peaks')
+	axes[0].plot(peak_freq, peak_amp, color=RED, marker='*', linestyle='None')
+	axes[0].grid(True, color='black', linestyle='dashed', alpha=0.2)
+	axes[0].set_xlabel('frequence in Hz')
+	axes[0].set_ylabel(r'amplitude')
+	axes[0].legend(loc='best')
+	axes[1].plot(data_table['k'], data_table[0], color=RED, marker='+', linestyle='None', label='Dispersionsrelation')
+	axes[1].grid(True, color='black', linestyle='dashed', alpha=0.2)
+	axes[1].set_xlabel('k in 1/m')
+	axes[1].set_ylabel(r'$\omega$')
+	axes[1].legend(loc='best')
+	plt.show()
+
 
 def find_peak(data, minimal_height):
 	indices = find_peaks(x=data, height=minimal_height)
@@ -121,33 +151,73 @@ polar_plot_labels = ['erste Resoananz 2308.764Hz',
 					 'vierte Resoananz 6237.197Hz',
 					 'fünfte Resoananz 6567.745Hz']
 
-def test():
-	# Fixing random state for reproducibility
-	np.random.seed(19680801)
+task4_1_list = ['1_4_0mm.dat',
+				'1_4_3mm.dat',
+				'1_4_6mm.dat',
+				'1_4_9mm.dat']
 
-	# Compute areas and colors
-	N = 150
-	r = 2 * np.random.rand(N)
-	theta = 2 * np.pi * np.random.rand(N)
-	area = 200 * r ** 2
-	colors = theta
+task4_labels = ['0mm', '3mm', '6mm', '9mm']
 
-	fig = plt.figure()
-	ax = fig.add_subplot(111, projection='polar')
-	c = ax.scatter(theta, r, c=colors, s=area, cmap='hsv', alpha=0.75)
-	plt.show()
+task4_2_list = ['1_4_0mm_l.dat',
+				'1_4_3mm_l.dat',
+				'1_4_6mm_l.dat',
+				'1_4_9mm_l.dat']
 
+task5_path_list = ['2_1_300mm.dat',
+				   '2_1_450mm.dat',
+				   '2_1_600mm.dat']
+
+task5_labels = ['300mm',
+				'450mm',
+				'600mm']
+
+task7_path_list = ['2_3_600_16.dat',
+				   '2_3_600_13.dat',
+				   '2_3_600_10.dat']
+
+task7_labels = ['Iris 16mm',
+				'Iris 13mm',
+				'Iris 10mm']
+
+task9_path_list = ['2_3_600_10_75_defekt_stelle_rechts.dat',
+				   '2_3_600_10_25_defekt_stelle_rechts.dat',
+				   '2_3_600_10_37_5_defekt_stelle_rechts.dat',
+				   '2_3_600_10_62_5_defekt_stelle_rechts.dat']
+
+task9_labels = ['Iris 10mm Stör 75mm',
+				'Iris 10mm Stör 25mm',
+				'Iris 10mm Stör 37.5mm',
+				'Iris 10mm Stör 62.5mm']
 
 # Aufgabe 1
-# show_spec('aufgabe1.dat', r'spectrum')
-# show_spec(task1_2_data_list) # peak control
-# peak_alpha_plot(task1_2_data_list) #TODO: correct alpha
+#show_spec_single('aufgabe1.dat', r'spectrum')
+# show_spec_single(task1_2_data_list) # peak control
+#peak_alpha_plot(task1_2_data_list) #TODO: correct alpha
 
 # Aufgabe 2
-# show_spec(task2_list, task2_labels)
-#show_spec('alpha_0_5000.dat', r'$\alpha=0\degree$' '\nhigh reslution')
+#show_spec_list(task2_list, task2_labels)
+#show_spec_single('alpha_0_5000.dat', r'$\alpha=0\degree$' '\nhigh reslution')
 
 # Aufgabe 3
 #show_polar_plots(polar_plot_list, polar_plot_labels) #TODO: rotate, order
 
 # Aufgabe 4
+#show_spec_list(task4_1_list, task4_labels)
+#show_spec_list(task4_2_list, task4_labels)
+
+# Aufgabe 5
+#show_spec_list(task5_path_list, task5_labels)
+
+# Aufgabe 6
+#show_spec_with_peaks('2_2_600.dat')
+
+# Aufgabe 7
+#show_spec_list(task7_path_list, task7_labels)
+
+# Aufgabe 8 #TODO: right?
+#show_spec_with_peaks('2_3_600_16.dat')
+#show_spec_with_peaks('2_3_600_13.dat')
+#show_spec_with_peaks('2_3_600_10.dat')
+
+# Aufgabe 9
+#show_spec_list(task9_path_list, task9_labels)
